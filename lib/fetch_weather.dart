61de27afb25eada53cb1553dc748f6157fc7dc32
Ravi Singh <40055802+imravi76@users.noreply.github.com>
2022-12-30 16:45:36 +0530

    Offline data fetching added

--- lib/fetch_weather.dart
+++ lib/fetch_weather.dart
@@ -1,4 +1,7 @@
 import 'dart:convert';
+
+import 'package:localstorage/localstorage.dart';
+
 import 'model/alert.dart';
 import 'model/weather_data.dart';
 import 'model/weather_data_current.dart';
@@ -11,17 +14,69 @@ class FetchWeatherAPI{
   String apiKey = "111e76f07e2c48a7d42b3fedbf8f9f4f";
   WeatherData? weatherData;
 
+  final storage = LocalStorage('weather_data.json');
+
   Future<WeatherData> processData(lat, lon) async{
+
+    var weatherData = await getWeatherFromCache(lat, lon);
+
+    if(weatherData == null){
+      return getDataFromAPI(lat, lon);
+    }
+
+    /*Directory appDocDir = await getApplicationDocumentsDirectory();
+    String appDocPath = appDocDir.path;
+    File file = File('$appDocPath/1.json');*/
+
+    /*file.writeAsString(response.body);
+    print("Write Successful");*/
+
+    return weatherData;
+  }
+
+  Future<WeatherData?> getWeatherFromCache(lat, lon) async {
+
+    await storage.ready;
+    var data = storage.getItem('weather_data');
+
+    if (data == null) {
+      return null;
+    } else {
+
+      Map<String, dynamic> dataNew = data;
+
+      weatherData = WeatherData(WeatherDataCurrent.fromJson(dataNew),
+          WeatherDataHourly.fromJson(dataNew),
+          WeatherDataDaily.fromJson(dataNew),
+          WeatherDataAlert.fromJson(dataNew));
+
+      getDataFromAPI(lat, lon);
+    }
+    //weatherData?.fromCache = true; //to indicate post is pulled from cache
+    return weatherData!;
+
+  }
+
+  Future<WeatherData> getDataFromAPI(lat, lon) async{
 
     var response = await http.get(Uri.parse(apiURL(lat, lon, apiKey)));
     var jsonString = jsonDecode(response.body);
+
     weatherData = WeatherData(WeatherDataCurrent.fromJson(jsonString),
         WeatherDataHourly.fromJson(jsonString),
-    WeatherDataDaily.fromJson(jsonString),
-    WeatherDataAlert.fromJson(jsonString));
+        WeatherDataDaily.fromJson(jsonString),
+        WeatherDataAlert.fromJson(jsonString));
+
+    saveWeather(jsonString!);
 
     return weatherData!;
   }
+
+  void saveWeather(jsonString) async {
+    await storage.ready;
+    storage.setItem("weather_data", jsonString);
+  }
+
 }
 
 String apiURL(var lat, var lon, var apiKey){
