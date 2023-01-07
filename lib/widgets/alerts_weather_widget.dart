35665a2e7859bd745893a6ad7764c8ec1cd57a23
Ravi Singh <40055802+imravi76@users.noreply.github.com>
2023-01-07 21:11:29 +0530

    Code Cleaning

--- lib/widgets/alerts_weather_widget.dart
+++ lib/widgets/alerts_weather_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../custom_colors.dart';
 import '../model/alert.dart';
-import 'alert_swipe.dart';
 
 class AlertsWeather extends StatefulWidget {
 
@@ -15,6 +16,7 @@ class AlertsWeather extends StatefulWidget {
 class _AlertsWeatherState extends State<AlertsWeather> {
   
   int lengthList = 0;
+  int _selectedPage = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -41,9 +43,105 @@ class _AlertsWeatherState extends State<AlertsWeather> {
             margin: const EdgeInsets.only(top: 1, left: 20, right: 20, bottom: 10),
             child: const Text("Alerts", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
           ),
-          AlertSwipe(alertList: widget.weatherDataAlert.alert),
+          alertSwipe(),
         ],
       );
     }
+  }
+
+  Widget alertSwipe(){
+    return Container(
+      height: 325,
+      margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: CustomColors.dividerLine.withAlpha(150)
+      ),
+      child: Row(
+        children: [
+
+          Expanded(
+            child: PageView(
+              scrollDirection: Axis.vertical,
+              onPageChanged: (num){
+                setState(() {
+                  _selectedPage = num;
+                });
+              },
+              children: [
+                for(var i=0; i<widget.weatherDataAlert.alert.length; i++)
+                  Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: const EdgeInsets.only(left: 10, top: 10),
+                        child: Text(
+                            "Sender Name: ${widget.weatherDataAlert.alert[i].senderName}"
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                            "Event: ${widget.weatherDataAlert.alert[i].event}"
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                            "Start: ${getDay(widget.weatherDataAlert.alert[i].start)}"
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                            "End: ${getDay(widget.weatherDataAlert.alert[i].end)}"
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: const EdgeInsets.only(left: 10, right: 20, bottom: 5),
+                        child: Text(
+                          "Description: ${widget.weatherDataAlert.alert[i].description}", textAlign: TextAlign.justify,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+              ],
+            ),
+          ),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for(var i=0; i<widget.weatherDataAlert.alert.length; i++)
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeOutCubic,
+                  width: 10.0,
+                  height: _selectedPage == i ? 35.0 : 10.0,
+                  decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(12.0)
+                  ),
+                  margin: const EdgeInsets.only(
+                      top: 2, right: 10
+                  ),
+                ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  String getDay(final timeStamp){
+    DateTime time = DateTime.fromMillisecondsSinceEpoch(timeStamp * 1000);
+    String x = DateFormat('EEEE').format(time);
+    return x;
   }
 }
